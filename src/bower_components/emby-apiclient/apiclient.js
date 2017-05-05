84cc1caa47d64d56e941a9b3ca05c67f1948f395
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-05-05 13:58:35 -0400

    update components

--- src/bower_components/emby-apiclient/apiclient.js
+++ src/bower_components/emby-apiclient/apiclient.js
@@ -143,39 +143,25 @@
 
         var currentServerInfo = this.serverInfo();
         var appName = this._appName;
-        var accessToken = currentServerInfo.AccessToken;
-
-        var values = [];
 
         if (appName) {
-            values.push('Client="' + appName + '"');
+
+            var auth = 'MediaBrowser Client="' + appName + '", Device="' + this._deviceName + '", DeviceId="' + this._deviceId + '", Version="' + this._appVersion + '"';
+
+            var userId = currentServerInfo.UserId;
+
+            if (userId) {
+                auth += ', UserId="' + userId + '"';
+            }
+
+            headers["X-Emby-Authorization"] = auth;
         }
 
-        if (this._deviceName) {
-            values.push('Device="' + this._deviceName + '"');
-        }
-
-        if (this._deviceId) {
-            values.push('DeviceId="' + this._deviceId + '"');
-        }
-
-        if (this._appVersion) {
-            values.push('Version="' + this._appVersion + '"');
-        }
+        var accessToken = currentServerInfo.AccessToken;
 
         if (accessToken) {
-            values.push('Token="' + accessToken + '"');
+            headers['X-MediaBrowser-Token'] = accessToken;
         }
-
-        if (values.length) {
-            
-            var auth = 'MediaBrowser ' + values.join(', ');
-            headers["Authorization"] = auth;
-        }
-
-        //if (accessToken) {
-        //    headers['X-MediaBrowser-Token'] = accessToken;
-        //}
     };
 
     ApiClient.prototype.appVersion = function () {

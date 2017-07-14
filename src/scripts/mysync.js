f44d674b2f309f7635615611d233a31f26918ff9
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-07-14 16:48:12 -0400

    update guide

--- src/scripts/mysync.js
+++ src/scripts/mysync.js
@@ -9,40 +9,12 @@
             return appHost.supports('sync') && params.mode == 'offline';
         }
 
-        function refreshSyncStatus(page) {
-
-            if (isLocalSyncManagement()) {
-
-                var status = localSync.getSyncStatus();
-
-                if (status == "Active") {
-                    page.querySelector('.btnSyncNow').classList.add('hide');
-                }
-                else {
-                    page.querySelector('.btnSyncNow').classList.remove('hide');
-                }
-            }
-        }
-
         function syncNow(page) {
 
             localSync.sync();
             require(['toast'], function (toast) {
                 toast(Globalize.translate('MessageSyncStarted'));
             });
-            refreshSyncStatus(page);
-        }
-
-        view.querySelector('.btnSyncNow').addEventListener('click', function () {
-            syncNow(view);
-        });
-
-        if (isLocalSyncManagement()) {
-
-            view.querySelector('.localSyncStatus').classList.remove('hide');
-
-        } else {
-            view.querySelector('.localSyncStatus').classList.add('hide');
         }
 
         var mySyncJobList = new syncJobList({
@@ -51,17 +23,6 @@
             userId: params.mode === 'offline' ? null : ApiClient.getCurrentUserId(),
             element: view.querySelector('.syncActivity'),
             mode: params.mode
-        });
-
-        view.addEventListener('viewbeforeshow', function () {
-
-            refreshSyncStatus(view);
-
-            if (appHost.supports('sync')) {
-                interval = setInterval(function () {
-                    refreshSyncStatus(view);
-                }, 5000);
-            }
         });
 
         view.addEventListener('viewbeforehide', function () {
